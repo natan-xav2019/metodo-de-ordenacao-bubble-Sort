2eb9138ff4324c226c98e552f52ae00b610cb414
Natan Xavier <55638524+natan-xav2019@users.noreply.github.com>
2020-05-13 10:38:17 -0300

    Update bubble sort - metodo de ordenação.c

--- bubble sort - metodo de ordenação.c
+++ bubble sort - metodo de ordenação.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 //bubble sort - metodo de ordenação
 void troca(int *valor1,int *valor2) {
@@ -11,28 +12,36 @@ void troca(int *valor1,int *valor2) {
 }
 
 void bubbleSort(int *prt_num, int max) {
-	int i, quanTroca=0;
+	int i,j=0, quanTroca=0,quantMaxT=0,quantMaxverificasoes=0;
 	
-	while(quanTroca!=1) {	
+	while(quanTroca!=1) {
 		quanTroca=0;
 		
-		for(i=0;i<max-1;i++) {
-			if(prt_num[i]>prt_num[i+1]) {
+		for(i=0;i<max-1-j;i++) {
+			if(prt_num[i]>=prt_num[i+1]) {
 					
 				troca(&prt_num[i],&prt_num[i+1]);
 				
 				quanTroca++;
 			}
 		}
+		quantMaxverificasoes+=i;
+		quantMaxT+=quanTroca;
+		j++;
 	}
+	printf("numero de interações = %i\n",j);
+	printf("quant Max verificações = %i\n", quantMaxverificasoes);
+	printf("quant Max Trocas = %i\n",quantMaxT);
 }
 
 main() {
 	int i,quanTroca=0,max=0;
 	int num,*prt_num = NULL, *prt_num_mais;
 	
+	setlocale(LC_ALL,"");
+	
 	printf("REGRA DE PARADA: 9999\n");
-	printf("digite quantos numeros desejar\n");
+	printf("digite quantos numeros desejar para ser ordenados\n");
 	
 	do {
 		scanf("%i",&num);
